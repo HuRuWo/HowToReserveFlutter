ea65e039490f1a10c0237197acf43837567a5fc2
liuw <1458476478@qq.com>
2022-06-02 16:16:14 +0800

    Signed-off-by: liuw <1458476478@qq.com>

--- FlutterCrackMe_01/android/app/src/main/java/com/example/fluttertest/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package com.example.fluttertest;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-
-}
--- FlutterCrackMe_01/lib/md5util.dart
+++
@@ -1,8 +0,0 @@
-import 'package:crypto/crypto.dart';
-import 'dart:convert';
-
-String getMd5String(String str){
-   var bytes = utf8.encode(str+"test2021");
-   var digest = md5.convert(bytes);
-   return digest.toString();
-}
